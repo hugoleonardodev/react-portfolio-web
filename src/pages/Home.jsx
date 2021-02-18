b711f1bd3d9adca50932d414ffadd284de9b4095
hugoleonardodev <hugoleonardo.dev@gmail.com>
2021-02-18 13:29:37 -0300

    test

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <section className="App-home">
+        Home section for Web Portfolio with React
+      </section>
+    );
+  }
+}
+
+export default Home;
